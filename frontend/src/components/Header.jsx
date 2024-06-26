e7c556f29439cd6a3e130ff826e6fd86818b04a2
dostloll <queenkingpotato@gmail.com>
2024-06-26 22:54:08 +1000

    Partially functional navbar with tailwind

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-scroll";
+import { FaTimes } from "react-icons/fa";
 import "./myStyle.css";
+import { CiMenuFries } from "react-icons/ci";
 
 export default function Header() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
   const content = (
     <>
-      <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 ppy-4">
-        <ul>
-          <link to="../About">
+      <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
+        <ul className="text-center text-xl p-20">
+          <link to="Home">
             <li>Home</li>
           </link>
           <link>
@@ -22,6 +29,7 @@ export default function Header() {
       </div>
     </>
   );
+
   return (
     <>
       <nav>
@@ -29,7 +37,7 @@ export default function Header() {
           <div className="flex items-center flex-1">
             <span>Logo</span>
           </div>
-          <div className="lg:flex md:flex lg: flex-1 items center justify-end">
+          <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
             <div className="flex-10">
               <ul className="flex gap-8 mr-16 text-[18px]">
                 <Link to="/">
@@ -47,6 +55,10 @@ export default function Header() {
               </ul>
             </div>
           </div>
+          <div>{click && content}</div>
+          <button className="block sm:hidden transition " onClick={handleClick}>
+            {click ? <FaTimes /> : <CiMenuFries />}
+          </button>
         </div>
       </nav>
     </>
